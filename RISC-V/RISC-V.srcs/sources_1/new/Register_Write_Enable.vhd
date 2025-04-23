72e78d74ef7c9d93a023eee215f5d00d65d1f705
htmos6 <mehmet.arslan4000@gmail.com>
2025-04-23 21:07:01 +0300

    register with Write Enable module implemented

---
+++ RISC-V/RISC-V.srcs/sources_1/new/Register_Write_Enable.vhd
@@ -0,0 +1,56 @@
+----------------------------------------------------------------------------------
+-- Engineer: Mehmet Arslan
+-- 
+-- Create Date: 04/23/2025 05:40:59 PM
+-- Design Name: Register_Write_Enable
+-- Module Name: Register_Write_Enable - RTL
+-- Project Name: Register_Write_Enable Module
+-- Target Devices: Xilinx FPGAs
+-- 
+-- 
+-- Revision: 0.0.0.1
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+use IEEE.NUMERIC_STD.all;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Register_Write_Enable is
+    generic (
+        W : integer range 1 to 64 := 32
+    );
+    port (
+        clk : in std_logic;
+        reset_i : in std_logic;
+        write_enable_i : std_logic;
+        data_i : in std_logic_vector(W - 1 downto 0);
+        data_o : out std_logic_vector(W - 1 downto 0)
+    );
+end entity Register_Write_Enable;
+
+architecture RTL of Register_Write_Enable is
+    signal data : std_logic_vector(W - 1 downto 0);
+begin
+
+    P_SEQUENTIAL : process (clk)
+    begin
+        if (rising_edge(clk)) then
+            if (reset_i = '1') then
+                data_o <= (others => '0');
+            elsif (write_enable_i = '1') then 
+                data_o <= data_i;
+                data <= data_i;
+            else
+                data_o <= data;    
+            end if;
+        end if;
+    end process;
+    
+end architecture RTL;
