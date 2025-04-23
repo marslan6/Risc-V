15aae60ffe0bcd47899789c3f1c8868c36ce5ea8
htmos6 <mehmet.arslan4000@gmail.com>
2025-04-23 21:05:03 +0300

    decoder2x4 syntax updated [LowerCase]

--- RISC-V/RISC-V.srcs/sim_1/new/TB_Decoder_2to4.vhd
+++ RISC-V/RISC-V.srcs/sim_1/new/TB_Decoder_2to4.vhd
@@ -13,74 +13,71 @@
 -- Additional Comments:
 -- 
 ----------------------------------------------------------------------------------
-LIBRARY IEEE;
-USE IEEE.STD_LOGIC_1164.ALL;
-USE IEEE.NUMERIC_STD.ALL;
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+use IEEE.NUMERIC_STD.all;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx leaf cells in this code.
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
-ENTITY TB_Decoder_2to4 IS
+entity TB_Decoder_2to4 is
     --  Port ( );
-END ENTITY TB_Decoder_2to4;
+end entity TB_Decoder_2to4;
 
-ARCHITECTURE RTL OF TB_Decoder_2to4 IS
+architecture RTL of TB_Decoder_2to4 is
+    signal tb_input_i : std_logic_vector(1 downto 0);
+    signal tb_output_o : std_logic_vector(3 downto 0);
+begin
 
-    SIGNAL tb_input_i : STD_LOGIC_VECTOR(1 DOWNTO 0);
-    SIGNAL tb_output_o : STD_LOGIC_VECTOR(3 DOWNTO 0);
-
-BEGIN
-
-    DUT_DECODER2X4 : ENTITY WORK.Decoder_2to4(RTL)
-        PORT MAP(
+    DUT_DECODER2X4 : entity WORK.Decoder_2to4(RTL)
+        port map(
             input_i => tb_input_i,
             output_o => tb_output_o
         );
 
-    P_COMBINATIONAL : PROCESS
-    BEGIN
+    P_COMBINATIONAL : process
+    begin
         ------------------------------------------------------------------
         ------------------------------------------------------------------
         ------------------------------------------------------------------
-        tb_input_i <= "00";  
-        WAIT FOR 2 ns;  
-        ASSERT (tb_output_o = "0001") REPORT "TEST : 0 [FAILED]" SEVERITY warning;
-
-        ------------------------------------------------------------------
-        ------------------------------------------------------------------
-        ------------------------------------------------------------------
-        tb_input_i <= "01";   
-        WAIT FOR 2 ns;   
-        ASSERT (tb_output_o = "0010") REPORT "TEST : 1 [FAILED]" SEVERITY warning;
-
-        ------------------------------------------------------------------
-        ------------------------------------------------------------------
-        ------------------------------------------------------------------
-        tb_input_i <= "10"; 
-        WAIT FOR 2 ns;     
-        ASSERT (tb_output_o = "0100") REPORT "TEST : 2 [FAILED]" SEVERITY warning;
-
-        ------------------------------------------------------------------
-        ------------------------------------------------------------------
-        ------------------------------------------------------------------
-        tb_input_i <= "11";  
-        WAIT FOR 2 ns;    
-        ASSERT (tb_output_o = "1000") REPORT "TEST : 3 [FAILED]" SEVERITY warning;
+        tb_input_i <= "00";
+        wait for 2 ns;
+        assert (tb_output_o = "0001") report "TEST : 0 [FAILED]" severity warning;
 
         ------------------------------------------------------------------
         ------------------------------------------------------------------
         ------------------------------------------------------------------
         tb_input_i <= "01";
-        WAIT FOR 2 ns;      
-        ASSERT (tb_output_o = "0010") REPORT "TEST : 4 [FAILED]" SEVERITY warning;
+        wait for 2 ns;
+        assert (tb_output_o = "0010") report "TEST : 1 [FAILED]" severity warning;
 
         ------------------------------------------------------------------
         ------------------------------------------------------------------
         ------------------------------------------------------------------
-        ASSERT (false) REPORT "ALL TESTS PASSED" SEVERITY failure;
+        tb_input_i <= "10";
+        wait for 2 ns;
+        assert (tb_output_o = "0100") report "TEST : 2 [FAILED]" severity warning;
 
-    END PROCESS;
+        ------------------------------------------------------------------
+        ------------------------------------------------------------------
+        ------------------------------------------------------------------
+        tb_input_i <= "11";
+        wait for 2 ns;
+        assert (tb_output_o = "1000") report "TEST : 3 [FAILED]" severity warning;
 
-END ARCHITECTURE RTL;
+        ------------------------------------------------------------------
+        ------------------------------------------------------------------
+        ------------------------------------------------------------------
+        tb_input_i <= "01";
+        wait for 2 ns;
+        assert (tb_output_o = "0010") report "TEST : 4 [FAILED]" severity warning;
+
+        ------------------------------------------------------------------
+        ------------------------------------------------------------------
+        ------------------------------------------------------------------
+        assert (false) report "ALL TESTS PASSED" severity failure;
+    end process;
+
+end architecture RTL;
--- RISC-V/RISC-V.srcs/sources_1/new/Decoder_2to4.vhd
+++ RISC-V/RISC-V.srcs/sources_1/new/Decoder_2to4.vhd
@@ -14,41 +14,39 @@
 -- 
 ----------------------------------------------------------------------------------
 
-LIBRARY IEEE;
-USE IEEE.STD_LOGIC_1164.ALL;
-USE IEEE.NUMERIC_STD.ALL;
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+use IEEE.NUMERIC_STD.all;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx leaf cells in this code.
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
-ENTITY Decoder_2to4 IS
-    PORT (
-        input_i : IN STD_LOGIC_VECTOR(1 DOWNTO 0);
-        output_o : OUT STD_LOGIC_VECTOR(3 DOWNTO 0)
+entity Decoder_2to4 is
+    port (
+        input_i : in std_logic_vector(1 downto 0);
+        output_o : out std_logic_vector(3 downto 0)
     );
-END ENTITY Decoder_2to4;
+end entity Decoder_2to4;
 
-ARCHITECTURE RTL OF Decoder_2to4 IS
-BEGIN
+architecture RTL of Decoder_2to4 is
+begin
 
-    P_COMBINATIONAL : PROCESS (input_i)
-    BEGIN
-
-        CASE (input_i) IS
-            WHEN "00" =>
+    P_COMBINATIONAL : process (input_i)
+    begin
+        case (input_i) is
+            when "00" =>
                 output_o <= "0001";
-            WHEN "01" =>
+            when "01" =>
                 output_o <= "0010";
-            WHEN "10" =>
+            when "10" =>
                 output_o <= "0100";
-            WHEN "11" =>
+            when "11" =>
                 output_o <= "1000";
-            WHEN OTHERS =>
+            when others =>
                 output_o <= "0000";
-        END CASE;
+        end case;
+    end process;
 
-    END PROCESS;
-
-END ARCHITECTURE RTL;
+end architecture RTL;
