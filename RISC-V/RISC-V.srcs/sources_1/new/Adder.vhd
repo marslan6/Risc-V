6739e63076cfed20859926cbdc562d0a1ff64bfd
htmos6 <mehmet.arslan4000@gmail.com>
2025-04-26 18:49:00 +0300

    Adder.vhd added

--- RISC-V/RISC-V.srcs/sources_1/new/Adder.vhd
+++ RISC-V/RISC-V.srcs/sources_1/new/Adder.vhd
@@ -1,17 +1,5 @@
 ----------------------------------------------------------------------------------
 -- Engineer: Mehmet Arslan
--- 
--- Create Date: 04/24/2025 10:42:45 PM
--- Design Name: Adder
--- Module Name: Adder - RTL
--- Project Name: Adder Module
--- Target Devices: Xilinx FPGAs
--- 
--- 
--- Revision: 0.0.0.1
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.all;
@@ -22,16 +10,16 @@ entity Adder is
         W : integer range 1 to 64 := 32
     );
     port (
-        input0_o : in std_logic_vector (W - 1 downto 0);
-        input1_o : in std_logic_vector (W - 1 downto 0);
+        input0_i : in std_logic_vector (W - 1 downto 0);
+        input1_i : in std_logic_vector (W - 1 downto 0);
         output_o : out std_logic_vector (W - 1 downto 0)
     );
 end entity Adder;
 
 architecture RTL of Adder is
 begin
-    P_COMBINATIONAL : process (input0_o, input1_o)
+    P_COMBINATIONAL : process (input0_i, input1_i)
     begin
-        output_o <= std_logic_vector(unsigned(input0_o) + unsigned(input1_o));
+        output_o <= std_logic_vector(unsigned(input0_i) + unsigned(input1_i));
     end process;
 end architecture RTL;
