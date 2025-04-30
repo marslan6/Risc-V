da98734ca0b8c549e87964596a402cd7cce3626a
htmos6 <mehmet.arslan4000@gmail.com>
2025-04-30 22:22:36 +0300

    decoder 5x32 effective implementation and shorter

--- RISC-V/RISC-V.srcs/sources_1/new/Decoder_5to32.vhd
+++ RISC-V/RISC-V.srcs/sources_1/new/Decoder_5to32.vhd
@@ -14,77 +14,15 @@ end entity Decoder_5to32;
 
 architecture RTL of Decoder_5to32 is
 begin
-
-    P_COMBINATIONAL : process (input_i)
+    P_COMBNATIONAL : process (input_i) is
+        variable value_to_shift : std_logic_vector(31 downto 0);
     begin
-        case (input_i) is
-            when x"00" =>
-                output_o <= x"00000001";
-            when x"01" =>
-                output_o <= x"00000002";
-            when x"02" =>
-                output_o <= x"00000004";
-            when x"03" =>
-                output_o <= x"00000008";
-            when x"04" =>
-                output_o <= x"00000010";
-            when x"05" =>
-                output_o <= x"00000020";
-            when x"06" =>
-                output_o <= x"00000040";
-            when x"07" =>
-                output_o <= x"00000080";
-            when x"08" =>
-                output_o <= x"00000100";
-            when x"09" =>
-                output_o <= x"00000200";
-            when x"0A" =>
-                output_o <= x"00000400";
-            when x"0B" =>
-                output_o <= x"00000800";
-            when x"0C" =>
-                output_o <= x"00001000";
-            when x"0D" =>
-                output_o <= x"00002000";
-            when x"0E" =>
-                output_o <= x"00004000";
-            when x"0F" =>
-                output_o <= x"00008000";
-            when x"10" =>
-                output_o <= x"00010000";
-            when x"11" =>
-                output_o <= x"00020000";
-            when x"12" =>
-                output_o <= x"00040000";
-            when x"13" =>
-                output_o <= x"00080000";
-            when x"14" =>
-                output_o <= x"00100000";
-            when x"15" =>
-                output_o <= x"00200000";
-            when x"16" =>
-                output_o <= x"00400000";
-            when x"17" =>
-                output_o <= x"00800000";
-            when x"18" =>
-                output_o <= x"01000000";
-            when x"19" =>
-                output_o <= x"02000000";
-            when x"1A" =>
-                output_o <= x"04000000";
-            when x"1B" =>
-                output_o <= x"08000000";
-            when x"1C" =>
-                output_o <= x"10000000";
-            when x"1D" =>
-                output_o <= x"20000000";
-            when x"1E" =>
-                output_o <= x"40000000";
-            when x"1F" =>
-                output_o <= x"80000000";
-            when others =>
-                output_o <= x"00000000";
-        end case;
+        value_to_shift := x"00000001";
+        output_o <= std_logic_vector(shift_left(
+                                                unsigned(value_to_shift), -- Number to shift. convert number to binary representation like "00001111"
+                                                to_integer(unsigned(input_i)) -- Shift amount. unsigned(input_i) gives a binary value like "00001111" 
+                                                                              -- not a number like 5 or x"0005"
+                                               )
+                                    );
     end process;
-
 end architecture RTL;
