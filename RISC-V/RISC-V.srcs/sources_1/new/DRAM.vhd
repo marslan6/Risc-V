2d711dd4fd333e3e4aaa9d47b78ac37a38d7f7cc
htmos6 <mehmet.arslan4000@gmail.com>
2025-05-02 23:13:44 +0300

    DRAM synthesize error solved

--- RISC-V/RISC-V.srcs/sources_1/new/DRAM.vhd
+++ RISC-V/RISC-V.srcs/sources_1/new/DRAM.vhd
@@ -10,31 +10,19 @@ use STD.TEXTIO.all;
 entity DRAM is
     generic (
         RAM_DEPTH : integer range 1 to 512 := 128;
-        RAM_WIDTH : integer range 1 to 64 := 32
+        RAM_WIDTH : integer range 1 to 64 := 32;
+        ADDR_WIDTH : integer range 1 to 9 := 7
     );
     port (
         clk : in std_logic;
         write_enable_i : in std_logic;
-        address_i : in std_logic_vector(log2(RAM_DEPTH) - 1 downto 0);
+        address_i : in std_logic_vector(ADDR_WIDTH - 1 downto 0);
         data_in_i : in std_logic_vector(RAM_WIDTH - 1 downto 0);
         data_out_o : out std_logic_vector(RAM_WIDTH - 1 downto 0)
     );
 end entity DRAM;
 
 architecture RTL of DRAM is
-    -- Define log2 function inside architecture
-    function log2(val : integer) return natural is
-        variable result : natural := 0;
-    begin
-        for i in 0 to 31 loop
-            if (val <= (2 ** i)) then
-                result := i;
-                exit;
-            end if;
-        end loop;
-        return result;
-    end function;
-
     -- RAM type declaration
     type t_distributed_ram is array (0 to RAM_DEPTH - 1) of std_logic_vector(RAM_WIDTH - 1 downto 0);
 
@@ -52,20 +40,17 @@ architecture RTL of DRAM is
     end function;
 
     -- RAM instance
-    signal RAM : t_distributed_ram := fill_ram_from_file("rams_init_file.data");
-    -- Resolved address input width
-    constant ADDR_WIDTH : natural := log2(RAM_DEPTH);
-
+    signal RAM : t_distributed_ram := fill_ram_from_file("rams_init_file.mem");
 begin
     -- Combinational read
-    data_out_o <= RAM(to_integer(unsigned(address_i(ADDR_WIDTH - 1 downto 0))));
+    data_out_o <= RAM(to_integer(unsigned(address_i)));
 
     -- Sequential write
     process (clk)
     begin
         if rising_edge(clk) then
             if (write_enable_i = '1') then
-                RAM(to_integer(unsigned(address_i(ADDR_WIDTH - 1 downto 0)))) <= data_in_i;
+                RAM(to_integer(unsigned(address_i))) <= data_in_i;
             end if;
         end if;
     end process;
