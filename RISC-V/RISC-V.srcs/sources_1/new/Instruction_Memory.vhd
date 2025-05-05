0d005614be8b4987212e0730f1ac7c20c8496c50
htmos6 <mehmet.arslan4000@gmail.com>
2025-05-05 21:51:13 +0300

    Instruction Mem - Async Read from DRAM

---
+++ RISC-V/RISC-V.srcs/sources_1/new/Instruction_Memory.vhd
@@ -0,0 +1,37 @@
+----------------------------------------------------------------------------------
+-- Engineer: Mehmet Arslan
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+use IEEE.NUMERIC_STD.all;
+
+entity Instruction_Memory is
+    generic (
+        RAM_DEPTH : integer range 1 to 512 := 128;
+        RAM_WIDTH : integer range 1 to 64 := 32;
+        ADDR_WIDTH : integer range 1 to 9 := 7
+    );
+    port (
+        address_i : in std_logic_vector(ADDR_WIDTH - 1 downto 0);
+        instruction_o : out std_logic_vector(RAM_WIDTH - 1 downto 0)
+    );
+end entity Instruction_Memory;
+
+architecture RTL of Instruction_Memory is
+begin
+
+    U_DRAM : entity work.DRAM(RTL)
+        generic map (
+            RAM_DEPTH => RAM_DEPTH,
+            RAM_WIDTH => RAM_WIDTH,
+            ADDR_WIDTH => ADDR_WIDTH
+        )
+        port map (
+            clk => '0',
+            write_enable_i => '0',
+            address_i => address_i,
+            data_in_i => (others => '0'),
+            data_out_o => instruction_o
+        );
+    
+end architecture RTL;
